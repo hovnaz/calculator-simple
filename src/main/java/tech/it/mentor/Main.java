9df4df8d2e45b52d5d54524f777c3c029e251dfb
Hovhannes Nazaryan <hovnaz.arm@gmail.com>
2024-01-11 17:26:12 +0400

    change in main calculator input argument

--- src/main/java/tech/it/mentor/Main.java
+++ src/main/java/tech/it/mentor/Main.java
@@ -5,7 +5,7 @@ public class Main {
     private static final Calculator calculator = new Calculator();
 
     public static void main(String[] args) {
-        System.out.println(calculator.calc("1110+2*345+10+10-10/100/10+0346.00545"));
+        System.out.println(calculator.calc("1110+2*345+10+10-10/100/10+346.00545"));
 //        System.out.println(calculator.calc("1110+003"));
 //        System.out.println(calculator.calc("11+10/2*345-1000"));
     }
